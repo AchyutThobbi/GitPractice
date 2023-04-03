353c79da267206e89759a7b13cba292d4668e469
AchyutThobbi <89890314+AchyutThobbi@users.noreply.github.com>
2023-04-03 16:04:23 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ class Hello
 {
 	public static void main ()
 	{
-		system.out.println("Hello, This is modified branch from feature 101 then changed from remote branch");
+		System.out.println("Hello, This is modified branch from feature 101 then changed from remote branch");
 	}
 }
